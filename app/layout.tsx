00b6cf6ec539140b695d82b803789e7d9cad5bde
Faisal Irfan <faisalirfan619@gmail.com>
2025-05-27 19:57:35 +0500

    fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light  ">
+        <ThemeProvider attribute="class" defaultTheme="light">
           <div className="flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
